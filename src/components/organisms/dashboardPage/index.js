a07cad091d1154e55d37fa460971f2543855f938
mochammadradhi <m.radhiakbar@gmail.com>
2022-09-03 22:57:34 +0700

    adding navbar and buttons, modified a layout

--- src/components/organisms/dashboardPage/index.js
+++ src/components/organisms/dashboardPage/index.js
@@ -1,19 +1,22 @@
 import { FaFacebook, FaSlack, FaWhatsapp, FaPinterestP } from "react-icons/fa";
+import { Navbar } from "../../atoms";
 const DashboardPage = (props) => {
   const { dataPortofolio, dataClient, dataAboutUs, dataTeam } = props;
-
+ 
   return (
     <>
+    <Navbar/>
       <section className="pt-24 pb-12  w-full h-[80vh] laptop:h-max">
         <div className="max-w-[1200px] flex flex-wrap laptop:flex-col mx-auto relative ">
           <div className=" laptop:basis-full laptop:mx-auto laptop:p-5 basis-1/3 ">
             <h1 className="text-4xl uppercase font-extrabold tracking-wide bg-[#3E5B5D] text-gray-200 p-4">
               discovery luxury interior design
             </h1>
-            <p className="text-sm mt-4 ">lorem ipsum asmet asdasd</p>
+            <p className="text-sm my-4 leading-tight tracking-wide font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum fugit dolore consequatur eos? Quaerat obcaecati veritatis cum eligendi temporibus explicabo!</p>
+            <button className="bg-[#3E5B5D] text-white font-medium p-3">About Us</button>          
           </div>
           <div className="laptop:basis-full basis-2/3 laptop:p-5">
-            <div className="rounded-t-full bg-contain bg-right-top h-[50vh] bg-no-repeat bg-[url('https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80')]"></div>
+            <div className="rounded-t-full bg-contain bg-right-top h-[50vh] bg-no-repeat bg-[url('https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80')] relative left-[-150px]"></div>
           </div>
           <div className="basis-1/3  mx-auto">
             <div className="flex flex-wrap mx-auto justify-evenly  text-2xl text-center  text-[#3E5B5D]">
@@ -39,12 +42,12 @@ const DashboardPage = (props) => {
         - impelementation responsive design
         - impelementation from wireframe
       */}
-      <section className="pt-24 pb-12 bg-[#273a3b]">
+      <section className="py-24  bg-[#273a3b]">
         <div className="max-w-[1200px] max-h-full mx-auto ">
           <h1 className="text-4xl laptop:mx-auto mb-6 laptop:p-4">
             PORTOFOLIO
           </h1>
-          <div className="flex gap-3 laptop:flex-col laptop:p-4 snap-mandatory">
+          <div className="flex gap-3 laptop:flex-col laptop:p-4 ">
             {dataPortofolio.map((data) => (
               <>
                 <div>
